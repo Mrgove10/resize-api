fe88bd74c5972a51e6adb19f2bcfa3944eb496e8
Adrien RICHARD <richardadrien0@gmail.com>
2021-07-16 00:09:22 +0200

    initial commit

---
+++ config.sample.js
@@ -0,0 +1,1 @@
+module.exports.rootUrl = 'http://xxxxx.com/users/high/'
---
+++ main.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const sharp = require('sharp')
+const axios = require('axios')
+const config = require('./config.js')
+
+// test urls :
+// http://localhost:3000/high/94466236.jpg
+// http://localhost:3000/low/94466236.jpg?w=100
+app.get('/:definition/:img', async (req, res) => {
+    const imgName = req.params.img;
+    const def = req.params.definition;
+    let widthParam = parseInt(req.query.w);
+    if (widthParam < 100) {
+        // 100 width, lowest reslution
+        widthParam = 100
+    }
+
+    //console.log(imgName, def, widthParam);
+
+    if (imgName && def) {
+        const originalURL = config.rootUrl + imgName;
+        if (def === "high") {
+            // if in high def redirect to the original url
+            res.redirect(originalURL);
+        }
+        else if (def === "low") {
+            // if the resolution is low, compress its
+            axios.get(originalURL, {
+                responseType: 'arraybuffer'
+            }).then((response) => {
+                const imgBuffer = Buffer.from(response.data, 'binary')
+                // console.log(imgBuffer);
+                sharp(imgBuffer)
+                    .resize({ width: widthParam })
+                    .toBuffer()
+                    .then(data => {
+                        var img = Buffer.from(data, 'base64');
+                        res.writeHead(200, {
+                            'Content-Type': 'image/png',
+                            'Content-Length': img.length
+                        });
+                        res.end(img);
+                    })
+                    .catch((err) => console.log(err));
+            });
+        }
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Resize api listening at http://localhost:${port}`)
+})
+
